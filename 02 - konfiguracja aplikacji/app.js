f2a9922190876adf7c76a83fbff681c4f258acb8
Adam Gospodarczyk <adam@overment.com>
2018-02-28 00:23:14 +0100

    02 - Application's configuration

---
+++ 02 - konfiguracja aplikacji/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const routes = require('./routes/index');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(flash());
+
+app.use('/', routes);
+
+module.exports = app;
