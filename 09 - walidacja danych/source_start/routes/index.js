d4e6e77b5631b0a2e4f777ce5245f34a4ca0d54f
Adam Gospodarczyk <adam@overment.com>
2018-03-11 21:06:44 +0100

    09 - walidacja danych

---
+++ 09 - walidacja danych/source_start/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const PagesController = require('../controllers/PagesController');
+const ApplicationsController = require('../controllers/ApplicationsController');
+const errorsHandler = require('../middlewares/errors');
+
+router.get('/', PagesController.home);
+
+router.post('/applications',
+    ApplicationsController.normalizeData,
+    errorsHandler.catchAsync(ApplicationsController.store)
+);
+
+module.exports = router;
