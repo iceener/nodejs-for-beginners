9f08dfe6b22e2fc8ba9bf8f661495a95de95bceb
Jakub Cholewka <mdcholewka@gmail.com>
2019-08-11 22:56:21 +0200

    Knexfile typo

--- 07 - obsluga bledow/source_end/config/bookshelf.js
+++ 07 - obsluga bledow/source_end/config/bookshelf.js
@@ -1,4 +1,4 @@
-const knex = require('knex')(require('./knexflie'));
+const knex = require('knex')(require('./knexfile'));
 const bookshelf = require('bookshelf')(knex);
 
 module.exports = bookshelf;
